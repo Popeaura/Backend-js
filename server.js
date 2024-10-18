bb8302bdf79573b9c28f62f583330a96a1aebe90
Popeaura <popeahura@gmail.com>
2024-10-19 02:55:20 +0300

    I modified my server.js file

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Nairobi');
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome to the landing page');
+    } else if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome to the about us page');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Page not found');
+    }
 });
 
-const PORT = 3000;
+const PORT = 4500;
 
 server.listen(PORT, () => {
-    console.log('My server is running.');
+    console.log('My server is running on port ' + PORT);
 });
